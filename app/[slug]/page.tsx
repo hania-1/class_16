dfa1a7db4039ddf072317924213fde6471166837
hania-1 <160575495+hania-1@users.noreply.github.com>
2024-12-30 22:14:24 +0500

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,22 +1,23 @@
-// import { SanityDocument } from "@sanity/client";
-// import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
-// import { sanityFetch } from "@/sanity/lib/fetch";
-// import { client } from "@/sanity/lib/client";
-// import Post from "../components/Post";
+import { SanityDocument } from "@sanity/client";
+import { postQuery } from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/fetch"; // This already uses the client internally
+import Post from "../components/Post";
 
-// export const revalidate = 60;
+export const revalidate = 60;
 
-// export async function generateStaticParams() {
-//   const posts = await client.fetch(postPathsQuery);
-//   return posts
-// }
+// Define the type for the params
+interface PostParams {
+  slug: string;
+}
 
-// const PostPage = async ({ params }: { params: any }) => {
-//   const post = await sanityFetch<SanityDocument>({ query: postQuery, params })
-//   return (
-//     <Post post={post} />
-//   )
-// }
+// Fetch the post data using the slug
+const PostPage = async ({ params }: { params: PostParams }) => {
+  const post = await sanityFetch<SanityDocument>({
+    query: postQuery,
+    params: { slug: params.slug }, // Use the slug to fetch the post
+  });
 
-// export default PostPage
+  return <Post post={post} />;
+};
 
+export default PostPage;
