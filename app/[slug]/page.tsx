ee3eae4103067f11c6760fd0b08387de1f7b2856
hania-1 <160575495+hania-1@users.noreply.github.com>
2024-12-30 22:04:07 +0500

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,22 +1,22 @@
-import { SanityDocument } from "@sanity/client";
-import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
-import { sanityFetch } from "@/sanity/lib/fetch";
-import { client } from "@/sanity/lib/client";
-import Post from "../components/Post";
+// import { SanityDocument } from "@sanity/client";
+// import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
+// import { sanityFetch } from "@/sanity/lib/fetch";
+// import { client } from "@/sanity/lib/client";
+// import Post from "../components/Post";
 
-export const revalidate = 60;
+// export const revalidate = 60;
 
-export async function generateStaticParams() {
-  const posts = await client.fetch(postPathsQuery);
-  return posts
-}
+// export async function generateStaticParams() {
+//   const posts = await client.fetch(postPathsQuery);
+//   return posts
+// }
 
-const PostPage = async ({ params }: { params: any }) => {
-  const post = await sanityFetch<SanityDocument>({ query: postQuery, params })
-  return (
-    <Post post={post} />
-  )
-}
+// const PostPage = async ({ params }: { params: any }) => {
+//   const post = await sanityFetch<SanityDocument>({ query: postQuery, params })
+//   return (
+//     <Post post={post} />
+//   )
+// }
 
-export default PostPage
+// export default PostPage
 
