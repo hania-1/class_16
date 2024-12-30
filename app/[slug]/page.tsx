e11bdfe116cbf4c1d1345240b75bc7ad1e689a19
hania-1 <160575495+hania-1@users.noreply.github.com>
2024-12-30 21:28:27 +0500

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -6,6 +6,11 @@ import Post from "../components/Post";
 
 export const revalidate = 60;
 
+// Define the type for the params
+interface PostParams {
+  slug: string;
+}
+
 export async function generateStaticParams() {
   const posts = await client.fetch(postPathsQuery);
   return posts.map((post: { slug: string }) => ({
@@ -13,7 +18,8 @@ export async function generateStaticParams() {
   }));
 }
 
-const PostPage = async ({ params }: { params: { slug: string } }) => {
+// Use the PostParams interface for the params prop
+const PostPage = async ({ params }: { params: PostParams }) => {
   const post = await sanityFetch<SanityDocument>({
     query: postQuery,
     params: { slug: params.slug },
